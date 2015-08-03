08605f4e40a7c1d429374ddaa97afe0670114f39
jsjeon <kevin.jeon@offact.com>
2015-08-04 01:00:35 +0900

    utube

--- src/main/webapp/js/auth.js
+++ src/main/webapp/js/auth.js
@@ -4,12 +4,13 @@
 // you need to register your own client ID.
 var OAUTH2_CLIENT_ID = '597261671284-7em4hfs25bd4a7c465abs1p55ius6qa1.apps.googleusercontent.com';
 var OAUTH2_SCOPES = [
-  'https://www.googleapis.com/auth/youtube'
+ 'https://www.googleapis.com/auth/youtube','https://www.googleapis.com/auth/plus.login','https://www.googleapis.com/auth/userinfo.email'
 ];
 
 // Upon loading, the Google APIs JS client automatically invokes this callback.
 googleApiClientReady = function() {
   gapi.auth.init(function() {
+	console.log('googleApiClientReady');
     window.setTimeout(checkAuth, 1);
   });
 }
@@ -20,6 +21,7 @@ googleApiClientReady = function() {
 // succeeds with no user intervention. Otherwise, it fails and the
 // user interface that prompts for authorization needs to display.
 function checkAuth() {
+	console.log('checkAuth');
   gapi.auth.authorize({
     client_id: OAUTH2_CLIENT_ID,
     scope: OAUTH2_SCOPES,
@@ -29,18 +31,20 @@ function checkAuth() {
 
 // Handle the result of a gapi.auth.authorize() call.
 function handleAuthResult(authResult) {
-	alert('authResult'+authResult);
+	console.log(authResult);
   if (authResult && !authResult.error) {
+	console.log('handleAuthResult 1');
     // Authorization was successful. Hide authorization prompts and show
     // content that should be visible after authorization succeeds.
     $('.pre-auth').hide();
     $('.post-auth').show();
     loadAPIClientInterfaces();
   } else {
+	 console.log('handleAuthResult 2');
     // Make the #login-link clickable. Attempt a non-immediate OAuth 2.0
     // client flow. The current function is called when that flow completes.
     $('#login-link').click(function() {
-   
+	  console.log('#login-link');
       gapi.auth.authorize({
         client_id: OAUTH2_CLIENT_ID,
         scope: OAUTH2_SCOPES,
@@ -49,6 +53,8 @@ function handleAuthResult(authResult) {
     });
   }
 }
+
+
 
 // Load the client interfaces for the YouTube Analytics and Data APIs, which
 // are required to use the Google APIs JS client. More info is available at
